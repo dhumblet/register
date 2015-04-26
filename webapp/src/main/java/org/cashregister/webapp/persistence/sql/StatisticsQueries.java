143fa29bd0982bf60eeb963e242d7fe733c453c9
Derk Humblet <derk.humblet@j4s.be>
2015-04-26 19:54:10 +0200

    Ignore negative transaction details in statistics

--- webapp/src/main/java/org/cashregister/webapp/persistence/sql/StatisticsQueries.java
+++ webapp/src/main/java/org/cashregister/webapp/persistence/sql/StatisticsQueries.java
@@ -17,16 +17,18 @@ public interface StatisticsQueries {
             "GROUP BY subresult.sdate\n" +
             "ORDER BY subresult.sdate ASC";
 
-        static String SALE_TURNOVER_QUERY = "" +
-        "SELECT subresult.sdate, SUM(subresult.price)\n" +
-        "FROM (\n" +
-        "\t SELECT t.id, t.price, DATE_FORMAT(t.date, ?1) AS sdate\n" +
-        "\t FROM transaction t\n" +
-        "\t WHERE t.merchantId = ?2\n" +
-        "\t\t AND t.date BETWEEN ?3 AND ?4\n" +
-        "\t\t AND t.truck = 0\n " +
-        "\t GROUP BY t.id, t.date\n" +
-        ") subresult\n" +
-        "GROUP BY subresult.sdate\n" +
-        "ORDER BY subresult.sdate ASC";
+    static String SALE_TURNOVER_QUERY = "" +
+            "SELECT subresult.sdate, SUM(subresult.price)\n" +
+            "FROM (\n" +
+            "\tSELECT t.id, SUM(td.price) AS price, DATE_FORMAT(t.date, ?1) AS sdate\n" +
+            "\tFROM transaction t\n" +
+            "\t\tJOIN transaction_detail td ON t.id = td.transaction\n" +
+            "\tWHERE t.merchantId = ?2\n" +
+            "\t\tAND t.date BETWEEN ?3 AND ?4\n" +
+            "\t\tAND t.truck = 0\n" +
+            "\t\tAND td.price > 0\n" +
+            "\tGROUP BY t.id, t.date\n" +
+            ") subresult\n" +
+            "GROUP BY subresult.sdate\n" +
+            "ORDER BY subresult.sdate ASC;";
 }
